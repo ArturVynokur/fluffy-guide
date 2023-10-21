262b22e2fcf072b3ce417f7778f64af0ecca8fe6
SITRIX <poptop2304@gmail.com>
2023-10-21 17:12:14 +0300

    ✨ Добавьте функционал сохранения предмета в базу данных
    Добавлен конструктор класса `Subject`, который инициализирует объект с переданными атрибутами:
    - `subject_id` - идентификатор предмета
    - `subject_name` - имя предмета
    
    Добавлен метод `save_to_database`, который выполняет вставку информации о предмете в базу данных. Метод использует переданный объект `db_manager` класса `DatabaseManager` для выполнения SQL-запроса `INSERT INTO subjects`. Значение для вставки берется из атрибута объекта `Subject`.

--- Subject.py
+++ Subject.py
@@ -1,10 +1,16 @@
 class Subject:
     def __init__(self, subject_id, subject_name):
+        # Конструктор класса инициализирует объект Subject с переданными атрибутами:
+        # subject_id - идентификатор предмета
+        # subject_name - имя предмета
         self.subject_name = subject_name
         self.subject_id = subject_id
 
     def save_to_database(self, db_manager):
+        # Метод save_to_database выполняет вставку информации о предмете в базу данных.
+        # Он использует переданный объект db_manager класса DatabaseManager
+        # для выполнения SQL-запроса INSERT INTO subjects.
+        # Значение для вставки берется из атрибута объекта Subject.
         query = "INSERT INTO subjects (subject_name) VALUES (%s)"
         values = (self.subject_name,)
         db_manager.execute_query(query, values)
-
